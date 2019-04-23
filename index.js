62e29cc02bd85b74707cf81cfe3edac4ebe98199
Jonas Holbech <jofh@kea.dk>
2019-04-23 10:38:48 +0200

    Template working

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-let myLink = "https://kea-alt-del.dk/t5/api/productlist";
+let myLink = "https://mywpsite.kea-alt-del.dk/wp-json/wp/v2/posts?_embed";
+const template = document.querySelector("template").content;
+const parent = document.querySelector("main");
+
 function loadData(link){
   fetch(link).then(e=>e.json()).then(data=>show(data))
 }
 
 function show(data){
-  data.forEach(object=>console.log(object))
+  data.forEach(object=>{
+    console.log(object)
+    //clone the template
+    const clone = template.cloneNode(true);
+
+    //populate it
+    const h1 = clone.querySelector("h1");
+    const section = clone.querySelector("section");
+    const p = clone.querySelector("p");
+    h1.textContent=object.title.rendered;
+    section.innerHTML=object.content.rendered;
+    p.innerHTML=object._embedded.author[0].name;
+    //append to DOM
+    //_embedded.author[""0""].name
+    parent.appendChild(clone)
+  })
 }
 loadData(myLink)
